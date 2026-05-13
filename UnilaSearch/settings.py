6200d1b035b9478bf8449beab2181aa9905a646d
nazmahevano <nazmahevano2397@gmail.com>
2026-05-13 11:46:23 +0700

    update

--- UnilaSearch/settings.py
+++ UnilaSearch/settings.py
@@ -149,6 +149,6 @@ CACHES = {
 
 CSRF_TRUSTED_ORIGINS = [
     'https://siktb.eng.unila.ac.id',
-    'http://127.0.0.1:8000',
-    'http://localhost:8000',
 ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
